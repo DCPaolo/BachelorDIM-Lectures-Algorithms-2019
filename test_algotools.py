6d269b815be14791611b33f81a4d1eeaa70b9be0
DCPaolo <dacosta.paolo@outlook.fr>
2019-09-30 11:58:59 +0200

    my test script

---
+++ test_algotools.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 09:33:01 2019
+
+@author: Paolo Da Costa
+"""
+
+import S1_algotools.py as algo
+import pytest
+
+def test_max_value():
+    tab_List=[1,2,3,4,6,7]
+    
+
+def test_reverse_table():
+    with pytest.raises(ZeroDivisionError):
+    
