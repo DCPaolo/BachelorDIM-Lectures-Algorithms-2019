4d179cc310b316c2d61cbb2809bdae0802470e07
DCPaolo <dacosta.paolo@outlook.fr>
2019-09-30 09:29:20 +0200

    commit pytest

--- S1_algotools.py
+++ S1_algotools.py
@@ -18,6 +18,7 @@ tab_List=[1,2,3,4,6,7]
 
 import numpy as np #numpy 
 import cv2
+import pytest
 #tab_zeros= np.zeros(12, dtype=np.int32)
 tab_From_List=np.array(tab_List)
     
@@ -49,37 +50,45 @@ print(max_value(tab_List))
    
            
 
-"""
+
 def reverse_table(tableau_Reverse):
-    
+    """
     Function who will reverse a table
-    
+    """
     if not(isinstance(tableau_Reverse, list)):         
         raise ValueError('max_value expected a list as input')
         
     buffer = len(tableau_Reverse)
     turns = int(len(tableau_Reverse)/2)
     
-    for i in range(turns):
-        buff_I = buffer-i
+    for i in range(0,turns):
+        buffer = buffer-1
         tmp=tableau_Reverse[i]
-        tableau_Reverse[i]=tableau_Reverse[buff_I]
-        tableau_Reverse[tableau_Reverse[buff_I]]=tmp
+        tableau_Reverse[i]=tableau_Reverse[buffer]
+        tableau_Reverse[buffer]=tmp
+        
     return tableau_Reverse
     
 print(reverse_table(tab_List))   
-"""
+
 def roi_bbox ():
-    matrix=np.zeros((10,10), dtype=np.int32)
     
-    matrix[3:6, 4:8]=np.ones((3,4), dtype=np.int32)
-    
-    for idrow in range (matrix.shape[0]):
-        for idcol in range (matrix.shape[1]):
-            pixVal=matrix[idrow, idcol]
     img=cv2.imread('pokeball.png',0)
     cv2.imshow('read image', img)
-    cv2.waitKey()      
+    cv2.waitKey()
+    compteur = 0
+    Coordonee_Haut = 0
+    Coordonee_Droit = 0
+    Coordonee_Gauche = 0
+    Coordonee_Bas = 0
+    
+    for idrow in range (img.shape[0]):
+        for idcol in range (img.shape[1]):
+            pix_Val=img[idrow, idcol]
+            if pix_Val != 0 and compteur == 0:
+                compteur = 1
+                print (idrow, idcol)
+          
     
 roi_bbox()
-           
+      
