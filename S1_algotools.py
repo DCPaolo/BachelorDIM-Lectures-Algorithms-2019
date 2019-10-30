6530d2ecf38588d21b229e233eef386bc11102fd
DCPaolo <dacosta.paolo@outlook.fr>
2019-10-30 11:35:15 +0100

    few change

--- S1_algotools.py
+++ S1_algotools.py
@@ -16,18 +16,20 @@ Paolo Da Costa
 
 
 import numpy as np #numpy 
-import cv2
+#import cv2
 tab_zeros= np.zeros(12, dtype=np.int32)
 #tab_From_List=np.array(tab_List)
 tab_List=[1,2,3,4,6,7]
     
-img=cv2.imread('pokeball.png',0)
+""" img=cv2.imread('pokeball.png',0)
 cv2.imshow('read image', img)
-cv2.waitKey()
+cv2.waitKey() """
     
-def max_value(tableau):
+def average_above_zero(tableau):
     """
-    Functions who will do the average of an array
+    Functions who will do the average of an array of positives values (int)
+    Args : Tableau : type array
+    return the var Som divided by N
     """
     
     N = 0
@@ -43,13 +45,33 @@ def max_value(tableau):
     return Som/N    
  
 
-print(max_value(tab_List)) 
+print(average_above_zero(tab_List)) 
            
+def max_value(tableau):
+    """
+    Function that return the max value and his index
+    Args: tableau: an Array:List value
+    
+    Returns the max value and his index
+    """
+    
+    maxVal = 0
+    index=0
+    
+    for id_tab in range(len(tableau)):
+               
+            if maxVal < tableau[id_tab] :
+                maxVal =  tableau[id_tab]
+                index = id_tab
+    return (maxVal,index)            
 
+print(max_value(tab_List))
 
 def reverse_table(tableau_Reverse):
     """
     Function who will reverse a table
+    Args : tableau_Reverse : an array : List Value
+    Returns the reverse table
     """
     if not(isinstance(tableau_Reverse, list)):         
         raise ValueError('max_value expected a list as input')
@@ -68,8 +90,11 @@ def reverse_table(tableau_Reverse):
 print(reverse_table(tab_List))  
  
 
-def roi_bbox (bounding_Box):
-    
+""" def roi_bbox (bounding_Box):
+    #Function who will find the boundind box of an picture
+    #Args : bouding_box : 2d array (pictures)
+    Returns the bouding box of numpy array
+    #
     
     Coordonee_Haut = 0
     Coordonee_Droit = 0
@@ -92,5 +117,5 @@ def roi_bbox (bounding_Box):
     return bounding_Box 
     
     
-roi_bbox(img)
+roi_bbox(img) """
   
